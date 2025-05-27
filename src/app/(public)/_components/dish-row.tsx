3c530a9871705ead6d104ea9d05d22fc8a5cb949
Pham Phu Loc <phamphuloc7663@gmail.com>
2025-05-27 11:35:13 +0700

    push

--- src/app/(public)/_components/dish-row.tsx
+++ src/app/(public)/_components/dish-row.tsx
@@ -70,7 +70,7 @@ export default function DishRow({ dish, session }: Props) {
             }
 
             if (!session?.customer) {
-              toast.error('Vui lòng quét mã QR để có thể đặt món!')
+              toast.error('Vui lòng quét mã QR để có thể đặt món')
             }
           }}
         >
